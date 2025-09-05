6ba7085964c55e124c8e06d1b0baf4c45b749952
Ildarc4S <ildarc4S@gmail.com>
2025-09-06 00:27:10 +0300

    Add layout 1 for color

--- sample.cc
+++ sample.cc
@@ -37,18 +37,23 @@ int main() {
   const char* vertex_shader_source = R"(
     #version 450 core
     layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+
+    out vec3 ourColor;
 
     void main() {
       gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+      ourColor = aColor;
     }
   )";
 
   const char* fragment_shader_source = R"(
     #version 450 core
     out vec4 FragColor;
+    in vec3 ourColor;
 
     void main() {
-      FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+      FragColor = vec4(ourColor, 1.0f);
     }
   )";
 
@@ -90,9 +95,9 @@ int main() {
   glDeleteShader(fragment_shader);
 
   float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
   };
 
   GLuint VBO, VAO;
@@ -103,9 +108,12 @@ int main() {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
+
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
   glBindVertexArray(0);
@@ -118,11 +126,13 @@ int main() {
 
     glUseProgram(shader_program);
     glBindVertexArray(VAO);
+
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
-  }
+}
+
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
